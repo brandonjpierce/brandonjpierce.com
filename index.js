ad3b9f261e36f7226ce20073b65194a1ca631597
Brandon Pierce <brandonpierce91@gmail.com>
2017-12-03 17:41:03 -0600

    Switch from now to up for static site hosting:

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 #!/usr/bin/env node
 const Listr = require('listr');
+const images = require('./tasks/images');
 const postcss = require('./tasks/postcss');
 const purify = require('./tasks/purify');
 const htmlmin = require('./tasks/htmlmin');
 
-const tasks = new Listr([{
-  title: 'Running PostCSS',
-  task: postcss,
-}, {
-  title: 'Running Minify HTML',
-  task: htmlmin,
-}, {
-  title: 'Running Purify',
-  task: purify,
-}]);
+const tasks = new Listr([
+  {
+    title: 'Moving Image Assets',
+    task: images,
+  },
+  {
+    title: 'Running PostCSS',
+    task: postcss,
+  },
+  {
+    title: 'Running Minify HTML',
+    task: htmlmin,
+  },
+  {
+    title: 'Running Purify',
+    task: purify,
+  },
+]);
 
 tasks.run().catch(err => console.log(err));
